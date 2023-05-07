4c6b81c675c3d6a097fee728f7b9172a69d69592
ozen-hub <info.ozenhub@gmail.com>
2023-05-07 23:34:23 +0530

    8

---
+++ src/main/java/com/devstack/pos/servlet/CustomerServlet.java
@@ -0,0 +1,22 @@
+package com.devstack.pos.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+}
